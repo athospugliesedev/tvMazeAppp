8fa4fc6cc94434d5b66325aa172aeb95c2c1ef00
athospugliesedev <athospugliesedev@gmail.com>
2023-04-20 22:38:07 -0300

    🚀

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { defaultTheme } from './src/common/constants/styles/theme/defaultTheme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ThemeProvider theme={defaultTheme}>
+      <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </ThemeProvider>
+    
   );
 }
 
